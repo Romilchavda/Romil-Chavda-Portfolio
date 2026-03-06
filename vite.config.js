f7cd1c25ac0a00cce3a93ff82c9aa0132cd2be4b
Romil <luharromil@gmail.com>
2026-03-06 20:21:36 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // GitHub Pages के लिए base path सेट करना ज़रूरी है 
-  // क्योंकि आपकी Repo का नाम 'Romil-Chavda-Portfolio' है
+
+
   base: '/Romilchavda/Romil-Chavda-Portfolio/',
   
   build: {
